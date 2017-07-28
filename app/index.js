e4f282e155befecc77d029cb1102aff2ea287f7b
Dennis Burger <dennis@dutchwebworks.nl>
2017-07-28 12:34:26 +0200

    Updated Yeomand script syntax and depedencies.

--- app/index.js
+++ app/index.js
@@ -1,12 +1,50 @@
 'use strict';
 
-var generators = require('yeoman-generator'),
+var Generator = require('yeoman-generator'),
 	mkdirp = require('mkdirp'),
 	yosay = require('yosay'),
 	chalk = require('chalk');
 
-module.exports = generators.Base.extend({
-	_createProjectFileSystem: function() {
+module.exports = class extends Generator {
+	initializing() {
+		var message = chalk.yellow.bold('Welcome to Dutwebworks Gulp ') + chalk.yellow('A starter kit for a web site with Gulp');
+		this.log(yosay(message, { maxLength: 17 }));
+	}
+
+	prompting() {
+		return this.prompt([{
+			type    : 'input',
+			name    : 'name',
+			message	: 'What is the name of this project?',
+			default : this.appname
+		}, {
+			type    : 'input',
+			name	: 'description',
+			message	: 'What is the project description?',
+			default : this.appname
+		}, {
+			type    : 'input',
+			name	: 'yourname',
+			message	: 'What is your name?',
+		}, {
+			type    : 'input',
+			name	: 'version',
+			message	: 'What is the version of your app?',
+			default	: '0.1.0'
+		}]).then((answers) => {
+			this.appname = answers.name;
+			this.appdescription = answers.description;
+			this.appauthor = answers.yourname;
+			this.youremail = answers.youremail;
+			this.appversion = answers.version;
+		});
+	}
+
+	configuring() {
+		this.config.save();
+	}
+
+	writing() {
 		var destRoot = this.destinationRoot(),
 			sourceRoot = this.sourceRoot(),
 			appDir = destRoot + '/httpdocs',
@@ -40,77 +78,25 @@ module.exports = generators.Base.extend({
 		this.fs.copy(sourceRoot + '/_Gulp_Windows.cmd', destRoot + '/_Gulp_Windows.cmd');	
 		this.fs.copy(sourceRoot + '/Gulpfile.js', destRoot + '/Gulpfile.js');
 
-		// License
-		this.fs.copy(sourceRoot + '/LICENSE', destRoot + '/LICENSE');
 
 		// Ignore files
 		this.fs.copy(sourceRoot + '/_gitignore', destRoot + '/.gitignore');
 		this.fs.copy(sourceRoot + '/_bowerrc', destRoot + '/.bowerrc');
 
 		// Template context aware files
+		this.fs.copyTpl(sourceRoot + '/LICENSE', destRoot + '/LICENSE', templateContext);
 		this.fs.copyTpl(sourceRoot + '/package.json', destRoot + '/package.json', templateContext);
 		this.fs.copyTpl(sourceRoot + '/bower.json', destRoot + '/bower.json', templateContext);
 		this.fs.copyTpl(sourceRoot + '/README.md', destRoot + '/README.md', templateContext);
 		this.fs.copyTpl(sourceRoot + '/httpdocs/index.html', appDir + '/index.html', templateContext);
-	},
-	_getPrompt: function() {
-		var prompts = [
-				{
-					name: 'name',
-					message: 'What is the name of the project?',
-					default: this.appname
-				},
-				{
-					name: 'description',
-					message: 'What is the project description?',
-				},
-				{
-					name: 'yourname',
-					message: 'What is your name?',
-				},
-				{
-					name: 'youremail',
-					message: 'What is your e-mail address?',
-				},
-				{
-					name: 'version',
-					message: 'What is the version of your app?',
-					default: '1.0.0'
-				}
-			];
+	}
 
-			return prompts;
-	},
-	_saveAnswers: function(answers, callback) {
-		this.appname = answers.name;
-		this.appdescription = answers.description;
-		this.appauthor = answers.yourname,
-		this.youremail = answers.youremail,
-		this.appversion = answers.version,
-		callback();
-	},
-	initializing: function() {
-		var message = chalk.yellow.bold('Welcome to Dutwebworks Gulp ') + chalk.yellow('A starter kit for a web site with Gulp');
-		this.log(yosay(message, { maxLength: 17 }));
-	},
-	promting: function() {
-		var done = this.async();
-
-		this.prompt(this._getPrompt(), function(answers){			
-			this._saveAnswers(answers, done);
-		}.bind(this));
-	},
-	configuring: function() {
-		this.config.save();
-	},
-	writing: function() {
-		this._createProjectFileSystem();
-	},
-	install: function() {
+	install() {
 		this.bowerInstall();
 		this.npmInstall();
-	},
-	end: function() {
+	}
+
+	end() {
 		this.spawnCommand('gulp', ['serve']);
 	}
-});
+};
